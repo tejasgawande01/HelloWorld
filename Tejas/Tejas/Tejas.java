c27e61c2f0fb96b3b81fcf85b0723d4bf53e36b9
tejas gawande <tejas gawande@DESKTOP-N64B9JF>
2025-01-09 17:13:40 +0530

    HelloTejas

---
+++ Tejas/Tejas/Tejas.java
@@ -0,0 +1,10 @@
+package Tejas;
+
+public class Tejas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Tejas/src/com/tejas/tejas.java
@@ -0,0 +1,11 @@
+package com.tejas;
+
+public class tejas {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Tejas");
+
+	}
+
+}
